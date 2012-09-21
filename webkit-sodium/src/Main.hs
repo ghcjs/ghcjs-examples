cd9accb54c29f74dba03235917190f659681a0d9
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-09-22 04:54:14 +1200

    Update freecell to use ghcjs-dom so it can run in a browser as well as natively

--- webkit-sodium/src/Main.hs
+++ webkit-sodium/src/Main.hs
@@ -26,39 +26,23 @@ import Graphics.UI.Gtk.WebKit.DOM.Node
 import Control.Monad
 import System.Random
 import FRP.Sodium
+import Graphics.UI.Gtk.WebKit.GHCJS (runWebGUI)
 
 -- Comments show how what these FFI calls should work when the
 -- code compiled is compiled with GHCJS
 main = do
-  initGUI
-  webView <- mainWebView               -- window
-  doc <- webViewGetDomDocument webView -- webView.document
-  Just body <- documentGetBody doc     -- doc.body
-  htmlElementSetInnerHTML body $       -- body.setInnerHTML
-    "Haskell Freecell"++
-    "<div style=\"position:relative;left:0px;top:0px;background-color:#e0d0ff;width:700px;height:500px\" "++
-    "id=\"test\" draggable=\"false\"></div>"
-  Just div <- fmap castToHTMLElement <$>
-    documentGetElementById doc "test"  -- doc.getElementById
-  unlisten <- engine doc div =<< mkFreecell
-  mainGUI
-  unlisten
-
--- On browser this function just needs to return "window"
-mainWebView = do
-  window <- windowNew
-  windowSetDefaultSize window 900 600
-  windowSetPosition window WinPosCenter
-  scrollWin <- scrolledWindowNew Nothing Nothing
-  webView <- webViewNew
-  window `containerAdd` scrollWin
-  scrollWin `containerAdd` webView
-  window `onDestroy` mainQuit
-  widgetShowAll window
-  return webView
-
-
-
+  unlisten <- runWebGUI $ \ webView -> do
+    doc <- webViewGetDomDocument webView -- webView.document
+    Just body <- documentGetBody doc     -- doc.body
+    htmlElementSetInnerHTML body $       -- body.setInnerHTML
+      "Haskell Freecell"++
+      "<div style=\"position:relative;left:0px;top:0px;background-color:#e0d0ff;width:700px;height:500px\" "++
+      "id=\"test\" draggable=\"false\"></div>"
+    Just div <- fmap castToHTMLElement <$>
+      documentGetElementById doc "test"  -- doc.getElementById
+    engine doc div =<< mkFreecell
+  -- unlisten
+  return ()
 
 
 
