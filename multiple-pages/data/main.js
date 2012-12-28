8dae3a22d26af7b6b63459aef02f2ef95b199f25
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-12-28 22:22:09 +1300

    Update .jsexe path to .trampoline.jsexe

--- multiple-pages/data/main.js
+++ multiple-pages/data/main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   goog.debug.Console.autoInstall();
-  $hs_loadPath = "../../bin/multiple-pages.jsexe/";
+  $hs_loadPath = "../../bin/multiple-pages.trampoline.jsexe/";
 
   // Must be called first
   $hs_init();
