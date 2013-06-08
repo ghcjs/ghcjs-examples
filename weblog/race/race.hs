52c12c62fe2d491cc78f93e1b3670238a7e524a2
Luite Stegeman <stegeman@gmail.com>
2013-06-08 03:42:44 +0200

    add temporary workaround for linker problem with race.hs

--- weblog/race/race.hs
+++ weblog/race/race.hs
@@ -6,6 +6,8 @@ import Control.Concurrent
 import GHCJS.Foreign
 import GHCJS.Types
 
+import Data.Text (pack)
+
 #ifdef __GHCJS__
 foreign import javascript unsafe "document.getElementById($1).style.left = '' + $2 + 'px'"
   setPos :: JSString -> Int -> IO ()
@@ -29,3 +31,4 @@ doRace str = go (0::Int)
              threadDelay 1
              go (n+1)
 
+x = pack "abc" -- workaround, sometimes the dependencies for text aren't linked without this, need to investigate
