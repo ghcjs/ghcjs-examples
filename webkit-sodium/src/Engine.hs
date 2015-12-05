23f4be9c74494a4f29b0b5f0d28f695270a0a073
Kostiantyn Rybnikov <k-bx@k-bx.com>
2015-12-05 19:49:07 +0200

    Build ghcjs-hello with stack

--- webkit-sodium/src/Engine.hs
+++ webkit-sodium/src/Engine.hs
@@ -1,37 +1,37 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP               #-}
+
 module Engine where
 
-import Prelude hiding ((!!))
-import GHCJS.DOM.Document
-       (documentCreateElement, documentGetElementById, documentGetBody,
-        documentGetDocumentElement)
-import GHCJS.DOM.HTMLElement
-       (htmlElementInsertAdjacentElement, htmlElementSetInnerHTML,
-        htmlElementInsertAdjacentHTML)
-import GHCJS.DOM (WebView, webViewGetDomDocument)
-import GHCJS.DOM.Types
-       (castToHTMLElement, castToElement, Document,
-        HTMLElement, IsElement, IsMouseEvent, gTypeElement)
-import Control.Applicative ((<$>))
-import Control.Arrow
-import Control.Monad.Trans ( liftIO )
-import GHCJS.DOM.Element
-import GHCJS.DOM.EventM
-import GHCJS.DOM.MouseEvent hiding (MouseEvent)
-import GHCJS.DOM.Node
-import Control.Monad
-import Control.Monad.State.Strict
-import Data.IORef
-import Data.Maybe
+import           Control.Applicative         ((<$>))
+import           Control.Arrow
+import           Control.Monad
+import           Control.Monad.State.Strict
+import           Control.Monad.Trans         (liftIO)
+import           Data.IORef
+import           Data.Maybe
+import           GHCJS.DOM                   (WebView, webViewGetDomDocument)
+import           GHCJS.DOM.Document          (createElement, getBody,
+                                              getDocumentElement,
+                                              getElementById)
+import           GHCJS.DOM.Element
+import           GHCJS.DOM.EventM
+import           GHCJS.DOM.HTMLElement       (insertAdjacentElement,
+                                              insertAdjacentHTML)
+import           GHCJS.DOM.MouseEvent        hiding (MouseEvent)
+import           GHCJS.DOM.Node
+import           GHCJS.DOM.Types             (Document, HTMLElement, IsElement,
+                                              IsMouseEvent, castToElement,
+                                              castToHTMLElement, gTypeElement)
+import           Prelude                     hiding ((!!))
 
-import FRP.Sodium
-import Game
-import Control.Monad.Reader (ReaderT(..))
+import           Control.Monad.Reader        (ReaderT (..))
+import           FRP.Sodium
+import           Game
 #ifdef MIN_VERSION_jsaddle
-import Language.Javascript.JSaddle
-        (js2, js1, js0, js, jsg, (!!), (#), (<#), fun,
-         deRefVal, JSValue(..), runJSaddle)
-import Control.Lens ((^.))
+import           Control.Lens                ((^.))
+import           Language.Javascript.JSaddle (JSValue (..), deRefVal, fun, js,
+                                              js0, js1, js2, jsg, runJSaddle,
+                                              (!!), ( # ), (<#))
 #endif
 
 -- Convert (game) world to/from screen co-ordinates.
@@ -60,40 +60,40 @@ fromWorldRect ((xOrig,yOrig),(wid,hei)) = (
 
 -- | Get the mouse position relative to the top-left corner of the specified
 -- HTML element.
-getXYRelativeTo :: (IsElement elt, IsMouseEvent e, IsElement t) =>
-                   Document -> elt -> EventM e t (Int, Int)
+getXYRelativeTo :: (IsElement elt, IsMouseEvent e) =>
+                   Document -> elt -> EventM t e (Int, Int)
 getXYRelativeTo doc container = do
     (px, py) <- mousePageXY doc container
     (cx, cy) <- liftIO $ elementPageXY doc container
     return (px - cx, py - cy)
 
-mousePageXY :: (IsElement elt, IsMouseEvent e, IsElement t) =>
-               Document -> elt -> EventM e t (Int, Int)
+mousePageXY :: (IsElement elt, IsMouseEvent e) =>
+               Document -> elt -> EventM t e (Int, Int)
 mousePageXY doc container = do
     (x, y) <- mouseClientXY
     liftIO $ do
-	Just body <- documentGetBody doc     -- doc.body
-        bodyScrollLeft <- elementGetScrollLeft body
-        bodyScrollTop  <- elementGetScrollTop body
-        Just docElt <- documentGetDocumentElement doc
-        docEltScrollLeft <- elementGetScrollLeft docElt
-        docEltScrollTop <- elementGetScrollTop docElt
+        Just body <- getBody doc     -- doc.body
+        bodyScrollLeft <- getScrollLeft body
+        bodyScrollTop  <- getScrollTop body
+        Just docElt <- getDocumentElement doc
+        docEltScrollLeft <- getScrollLeft docElt
+        docEltScrollTop <- getScrollTop docElt
         return (x + bodyScrollLeft + docEltScrollLeft, y + bodyScrollTop + docEltScrollTop)
 
 -- | Get the top/left position of this element relative to the page.
 elementPageXY :: IsElement elt => Document -> elt -> IO (Int, Int)
 elementPageXY doc elt = do
-    Just body <- documentGetBody doc
+    Just body <- getBody doc
     traverse body (castToElement elt) (0,0)
   where
     traverse body elt (x, y) = do
-        eq <- nodeIsEqualNode elt (Just body)
+        eq <- isEqualNode elt (Just body)
         if eq
             then return (x, y)
             else do
-                ox <- elementGetOffsetLeft elt
-                oy <- elementGetOffsetTop elt
-                Just parent <- elementGetOffsetParent elt
+                ox <- getOffsetLeft elt
+                oy <- getOffsetTop elt
+                Just parent <- getOffsetParent elt
                 traverse body parent (x + round ox, y + round oy)
 
 -- The game logic expects alternating down-up-down-up, but the browser can produce
@@ -118,7 +118,7 @@ engine webView containerId game = do
     putStrLn "Haskell Freecell"
 
     Just doc <- webViewGetDomDocument webView
-    Just container <- fmap castToHTMLElement <$> documentGetElementById doc containerId
+    Just container <- fmap castToHTMLElement <$> getElementById doc containerId
     -- Construct a mouse event that lives in FRP land, and a push action
     -- that allows us to push values into it from IO land.
     (eMouse, pushMouse) <- sync newEvent
@@ -126,13 +126,13 @@ engine webView containerId game = do
     sanitize <- mkSanitize Up
 
     -- Listen to mouse events from WebKit
-    elementOnmousedown container $ do
+    on container mouseDown $ do
         xy <- getXYRelativeTo doc container
         liftIO . sanitize Down . sync . pushMouse . MouseDown . toWorld $ xy
-    elementOnmousemove container $ do
+    on container mouseMove $ do
         xy <- getXYRelativeTo doc container
         liftIO . sync . pushMouse . MouseMove . toWorld $ xy
-    elementOnmouseup container $ do
+    on container mouseUp $ do
         xy <- getXYRelativeTo doc container
         liftIO . sanitize Up . sync . pushMouse . MouseUp . toWorld $ xy
 
@@ -153,36 +153,43 @@ engine webView containerId game = do
         c ^. addEventListener "touchstart" (fun $ \f this [e] -> do
           e ^. preventDefault
           n <- e ^. touches . jsLength >>= deRefVal
-          when (n == ValNumber 1) $ do
-              t <- (e ^. touches) !! 0
-              vx <- t ^. pageX >>= deRefVal
-              vy <- t ^. pageY >>= deRefVal
-              case (vx, vy) of
-                (ValNumber x, ValNumber y) ->
-                  liftIO . sanitize Down . sync . pushMouse . MouseDown . toWorld $ ((floor x)-cx, (floor y)-cy)
-                _ -> return ())
+          case n of
+              ValNumber 1 -> do
+                t <- (e ^. touches) !! 0
+                vx <- t ^. pageX >>= deRefVal
+                vy <- t ^. pageY >>= deRefVal
+                case (vx, vy) of
+                  (ValNumber x, ValNumber y) ->
+                    liftIO . sanitize Down . sync . pushMouse . MouseDown . toWorld
+                      $ ((floor x)-cx, (floor y)-cy)
+                  _ -> return ()
+              _ -> return ())
         c ^. addEventListener "touchmove" (fun $ \f this [e] -> do
           e ^. preventDefault
           n <- e ^. touches . jsLength >>= deRefVal
-          when (n == ValNumber 1) $ do
-              t <- (e ^. touches) !! 0
-              vx <- t ^. pageX >>= deRefVal
-              vy <- t ^. pageY >>= deRefVal
-              case (vx, vy) of
-                (ValNumber x, ValNumber y) ->
-                  liftIO . sync . pushMouse . MouseMove . toWorld $ ((floor x)-cx, (floor y)-cy)
-                _ -> return ())
+          case n of
+              ValNumber 1 -> do
+                t <- (e ^. touches) !! 0
+                vx <- t ^. pageX >>= deRefVal
+                vy <- t ^. pageY >>= deRefVal
+                case (vx, vy) of
+                  (ValNumber x, ValNumber y) ->
+                    liftIO . sync . pushMouse . MouseMove . toWorld $ ((floor x)-cx, (floor y)-cy)
+                  _ -> return ()
+              _ -> return ())
         c ^. addEventListener "touchend" (fun $ \f this [e] -> do
           e ^. preventDefault
           n <- e ^. changedTouches . jsLength >>= deRefVal
-          when (n == ValNumber 1) $ do
-              t <- (e ^. changedTouches) !! 0
-              vx <- t ^. pageX >>= deRefVal
-              vy <- t ^. pageY >>= deRefVal
-              case (vx, vy) of
-                (ValNumber x, ValNumber y) ->
-                  liftIO . sanitize Up . sync . pushMouse . MouseUp . toWorld $ ((floor x)-cx, (floor y)-cy)
-                _ -> return ())
+          case n of
+              ValNumber 1 -> do
+                t <- (e ^. changedTouches) !! 0
+                vx <- t ^. pageX >>= deRefVal
+                vy <- t ^. pageY >>= deRefVal
+                case (vx, vy) of
+                  (ValNumber x, ValNumber y) ->
+                    liftIO . sanitize Up . sync . pushMouse . MouseUp . toWorld $ ((floor x)-cx, (floor y)-cy)
+                  _ -> return ()
+              _ -> return ())
 #endif
 
     -- Instantiate the FRP logic: We give it our mouse event, and it gives us back the
@@ -218,15 +225,15 @@ showAll doc container sprites =
             -- Remember the behaviour's last value
             lastRef <- newIORef []
             -- Listen to changes in the value of the behaviour (a list of sprites)
-            unlisten <- sync $ listen (values beh) $ \these -> do
+            unlisten <- sync $ listen (value beh) $ \these -> do
                 last <- readIORef lastRef
-                let toAdd = drop (length last) these
-                    toRemove = drop (length these) (map fst last)
+                let toAdd = Prelude.drop (length last) these
+                    toRemove = Prelude.drop (length these) (map fst last)
                     toModify = zip these last
                     keptImgs = map (\(_, (elt, _)) -> elt) toModify
                     noOfKeptImgs = length keptImgs
                 -- If there are fewer elements than last time, we delete the excess ones.
-                mapM_ (nodeRemoveChild container . Just) toRemove
+                mapM_ (removeChild container . Just) toRemove
                 -- If the first n sprites exist in both the old and new list, we
                 -- re-use them, and just modify their position and appearance as necessary
                 sequence_ $ zipWith (\zIx (newSprite, (elt, oldSprite)) -> do
@@ -237,11 +244,11 @@ showAll doc container sprites =
                     ) [zIxRoot..] toModify
                 -- If there are more elements than last time, we create the new ones
                 addedImgs <- sequence $ zipWith (\zIx sprite@(rect, fn) -> do
-                        Just elt <- fmap castToHTMLElement <$> documentCreateElement doc "img"
+                        Just elt <- fmap castToHTMLElement <$> createElement doc (Just "img")
                         position elt rect zIx
                         associate elt fn
-                        elementSetAttribute elt "ondragstart" "return false"
-                        nodeAppendChild container (Just elt)
+                        setAttribute elt "ondragstart" "return false"
+                        appendChild container (Just elt)
                         return elt
                     ) [zIxRoot + noOfKeptImgs..] toAdd
                 writeIORef lastRef (zip (keptImgs ++ addedImgs) these)
@@ -249,9 +256,9 @@ showAll doc container sprites =
     -- Set the necessary attributes to position the image according to the specified
     -- rectangle
     position elt rect zIx = do
-        elementSetAttribute elt "style" (style origin zIx)
-        elementSetAttribute elt "width" (show wid)
-        elementSetAttribute elt "height" (show hei)
+        setAttribute elt "style" (style origin zIx)
+        setAttribute elt "width" (show wid)
+        setAttribute elt "height" (show hei)
       where
         (origin, (wid, hei)) = fromWorldRect rect
         style (x, y) zIx = "position:absolute;top:"++show y
@@ -268,5 +275,5 @@ showAll doc container sprites =
             "user-select: none"
     -- Associate the image element with a URL, i.e. set its appearance.
     associate elt fn =
-        elementSetAttribute elt "src" ("http://ghcjs.github.com/"++fn)
+        setAttribute elt "src" ("http://ghcjs.github.com/"++fn)
 
